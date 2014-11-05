74ab83bff98c5c62fb6ab921ae754850fea238df
reancris <marcos_car_24@hotmail.com>
2014-11-05 00:42:25 -0500

    update

--- ControlAsistencia/Presentacion/controlasistencia/js/application.js
+++
@@ -1,4 +0,0 @@
-$('[data-toggle="confirmation"]').confirmation();
-$('[data-toggle="confirmation-singleton"]').confirmation({ singleton: true });
-$('[data-toggle="confirmation-popout"]').confirmation({ popout: true });
-
